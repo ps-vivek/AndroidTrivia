57fe9e81e2033a351005ab39f9d9f7150e7ef1aa
Vim <36118616+ps-vivek@users.noreply.github.com>
2021-07-11 11:37:37 -0700

    Preventing nav gesture

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -42,7 +42,14 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView, navController)
-
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
     }
